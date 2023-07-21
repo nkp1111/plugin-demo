450ff1117378e7fa68ae4d12e5203fd6c248d7fd
nkp1111 <neerajp4321@gmail.com>
2023-07-21 10:52:29 +0530

    feat: send server data

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const cors = require('cors');
+
 
 const ejs = require('ejs');
 
 const plugins = {
-  googleAnalytics: { installed: true, key: { measurementId: process.env.ANALYTIC_ID } },
+  googleAnalytics: { installed: true, key: process.env.ANALYTIC_ID },
 }
 
-port = 3000;
+const port = 3000;
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.use(cors('*'));
 
-app.get('/', async (req, res) => {
+app.get('/api', async (req, res) => {
   const data = await ejs.renderFile('./views/index.ejs', { plugins });
   // console.log(data) 
-  res.render('index.ejs', { plugins });
+  // res.render('index.ejs', { plugins });
+  res.json({ plugins });
 })
 
-app.get('/install', (req, res) => {
+app.get('/api/install', (req, res) => {
   const { plugin } = req.query;
   plugins[plugin].installed = true;
-  res.redirect('/');
+  res.redirect('/api');
 })
 
-app.get('/uninstall', (req, res) => {
+app.get('/api/uninstall', (req, res) => {
   const { plugin } = req.query;
   plugins[plugin].installed = false;
-  res.redirect('/');
+  res.redirect('/api');
+})
+
+app.get("/api/addApi", (req, res) => {
+  const { plugin, apiKey } = req.query;
+  plugins[plugin].key = apiKey;
+  res.redirect('/api');
 })
 
 app.listen(port, () => {
