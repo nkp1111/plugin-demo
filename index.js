c2b19f4b458c54df6f4b6ddbad609a4cecc9131d
nkp1111 <neerajp4321@gmail.com>
2023-07-25 12:46:14 +0530

    feat: minor tweaks to server

--- index.js
+++ index.js
@@ -6,39 +6,63 @@ const cors = require('cors');
 
 const ejs = require('ejs');
 
-const plugins = {
-  googleAnalytics: { installed: true, key: process.env.ANALYTIC_ID },
+const users = {
+  user1: {
+    plugins: {
+      googleAnalytics: { installed: true, key: process.env.GOOGLE_ANALYTIC_ID1 },
+    }
+  },
+  user2: {
+    plugins: {
+      googleAnalytics: { installed: true, key: process.env.GOOGLE_ANALYTIC_ID2 },
+    }
+  },
 }
 
 const port = 3000;
+const serverUrl = 'http://localhost:3000'
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(cors('*'));
 
-app.get('/api', async (req, res) => {
-  const data = await ejs.renderFile('./views/index.ejs', { plugins });
-  // console.log(data) 
-  // res.render('index.ejs', { plugins });
-  res.json({ plugins });
+app.get('/api/:user', async (req, res) => {
+  const { user } = req.params;
+  const pluginData = users[user].plugins;
+
+  const pluginInstallUninstall = (plugin, action = "install") => {
+    fetch(`${serverUrl}/api/${user}/${action}?plugin=${plugin}`, {
+      method: 'POST',
+    })
+      .then(() => window.location.reload())
+      .catch(err => console.log(err))
+  }
+  const data = await ejs.renderFile('./views/index.ejs', { plugins: pluginData, user, serverUrl, pluginInstallUninstall });
+  // res.send(data);
+
+  res.json({ plugins: pluginData, data });
 })
 
-app.get('/api/install', (req, res) => {
+app.post('/api/:user/install', (req, res) => {
   const { plugin } = req.query;
-  plugins[plugin].installed = true;
-  res.redirect('/api');
+  const { user } = req.params;
+  users[user].plugins[plugin].installed = true;
+  res.redirect('/api/' + user);
 })
 
-app.get('/api/uninstall', (req, res) => {
+app.post('/api/:user/uninstall', (req, res) => {
   const { plugin } = req.query;
-  plugins[plugin].installed = false;
-  res.redirect('/api');
+  const { user } = req.params;
+  console.log(plugin, user)
+  users[user].plugins[plugin].installed = false;
+  res.redirect('/api/' + user);
 })
 
-app.get("/api/addApi", (req, res) => {
+app.get("/api/:user/addApi", (req, res) => {
   const { plugin, apiKey } = req.query;
-  plugins[plugin].key = apiKey;
-  res.redirect('/api');
+  const { user } = req.params;
+  users[user].plugins[plugin].key = apiKey;
+  res.redirect('/api/' + user);
 })
 
 app.listen(port, () => {
